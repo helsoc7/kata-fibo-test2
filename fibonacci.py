6ea26aab5a1a83ade3c7b6cc5c643684f07eabc0
Helen Haveloh <120733305+helsoc7@users.noreply.github.com>
2023-11-14 13:12:40 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,7 @@
 def fibonacci(n):
-    fib_sequence = [0, 1]  # Die ersten beiden Fibonacci-Zahlen
+    fib_sequence = [0, 1]
 
     while len(fib_sequence) < n:
-        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        fib_sequence.append(sum(fib_sequence[-2:]))
 
-    # Die Liste der Fibonacci-Zahlen in einen kommaseparierten String umwandeln
-    fib_str = ', '.join(map(str, fib_sequence[:n]))
-
-    return fib_str
+    return fib_sequence[:n]
